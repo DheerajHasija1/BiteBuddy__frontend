5997e0643d62d9a2db91afe8c50e05322ccf6e4f
Dheeraj Hasija <dheerajrajpura0@gmail.com>
2025-10-11 20:10:57 +0530

    fix responsive  for mobile

--- src/Component/Home/Carouselitems.jsx
+++ src/Component/Home/Carouselitems.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const CarouselItem = ({image,title}) => {
 return (
-    <div className='flex flex-col justify-center items-center'>
-        <img className='w-[10rem] h-[10rem] 1g:h-[14rem] lg:w-[14rem] rounded-full object-cover object-center' src={image} alt="" />
-        <span className='py-5 font-semibold text-xl |text-gray-400'>{title}</span>
+    <div className='flex flex-col justify-center items-center px-2'>
+        <img className='w-[8rem] h-[8rem] sm:w-[10rem] sm:h-[10rem] lg:h-[14rem] lg:w-[14rem] rounded-full object-cover object-center' src={image} alt={title} />
+        <span className='py-3 sm:py-5 font-semibold text-base sm:text-xl text-gray-400'>{title}</span>
     </div>
 )
 }
--- src/Component/Home/MultiItemCarousel.jsx
+++ src/Component/Home/MultiItemCarousel.jsx
@@ -14,7 +14,37 @@ const MultiItemCarousel = () => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     };
     return (
         <div>
