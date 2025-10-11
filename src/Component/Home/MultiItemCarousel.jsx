33f423a7c6cfe01dbd132f782269f0b8a6a90876
Dheeraj Hasija <dheerajrajpura0@gmail.com>
2025-10-11 20:22:36 +0530

    response for mobile

--- src/Component/Home/MultiItemCarousel.jsx
+++ src/Component/Home/MultiItemCarousel.jsx
@@ -7,45 +7,59 @@ import { topMeals } from "./topMeal";
 
 const MultiItemCarousel = () => {
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1536, // 2xl screens
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
             }
-        ]
-    };
+        },
+        {
+            breakpoint: 1280, // xl screens
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 1024, // lg screens (tablets landscape)
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768, // md screens (tablets)
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 640, // sm screens (large phones)
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480, // xs screens (small phones)
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+};
     return (
         <div>
             <Slider {...settings}>
